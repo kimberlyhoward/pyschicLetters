7649910660e435010b6d3adc824f2ef6db296baf
Kimberly Howard <Kimberly Howard>
2018-01-31 17:52:16 -0700

    Java

---
+++ assets/game.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+var losses = 0;
+
+document.onkeyup = function (event) {
+
+    // Determines which key was pressed.
+    var userGuess = event.key;
+
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+    
+</script>
