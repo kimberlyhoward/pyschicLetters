66368d8609018d1fbce3d20a4e68efa86b69528a
Kimberly Howard <Kimberly Howard>
2018-02-03 21:35:39 -0700

    JS

--- assets/game.js
+++ assets/game.js
@@ -1,18 +1,40 @@
-<script type="text/javascript">
-
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-var wins = 0;
-var losses = 0;
-
 document.onkeyup = function (event) {
 
-    // Determines which key was pressed.
-    var userGuess = event.key;
+    //ctrl+r to refresh page, but also to set lettersGuessed by player to ''.
+    //the 'r' was being recorded as a guess.
+    if (event.keyCode == 73 && event.ctrlKey) {
+        startGame();
+    }
 
-    // Randomly chooses a choice from the options array. This is the Computer's guess.
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    //allows only A-Z on keyboard to be registered as answers
+    else if (event.keyCode >= 65 && event.keyCode <= 90) {
 
+        var key = event.key;
+        console.log(computerChoice);
+        if (guesses != 0) {
+            lettersGuessed += key + ', ';
+            if (key == computerChoice) {
+                wins++;
+                display.innerHTML = "You WIN! The computer chose " + computerChoice +
+                    "<br/> To play again press any button.";
+                startGame();
+            }
+            else if (key != computerChoice && guesses != 0) {
+                guesses--;
+                displayResults('Keep Guessing!', key, 'Not Telling!', guesses);
+            }
+        }
+        else if (guesses == 0) {
+            losses++;
+            display.innerHTML = "You LOSE! The computer chose: " + computerChoice +
+                "<br/> To play again press any button.";
+            startGame();
+        }
+    }
 
-    
-</script>
+    //this runs when ctrl+r is hit. shouldn't this not run? for some reason its reading the ctrl part
+    //when I press ctrl+r.
+    else {
+        alert("Only A-Z allowed.");
+    }
+}
